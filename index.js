be2ed69de383ad3c3281c92ae9cb3b6e21d4fdd7
Tommy Goode <tommy@tgoode.com>
2017-05-26 04:19:50 -0500

    Changing the stdout setting so that the spawned process has its own stream for standard output. By returning the spawned ChildProcess, the task output stream can be accessed/redirected (e.g. in a Gulp task).

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ module.exports = {
 
   dump : function(opts) {
     var spawn = new Spawn("mongodump", opts);
+    return spawn;
   },
 
   restore : function(opts) {
     var spawn = new Spawn("mongorestore", opts);
+    return spawn;
   },
 
   export : function(opts) {
     var spawn = new Spawn("mongoexport", opts);
+    return spawn;
   },
 
   import : function(opts) {
     var spawn = new Spawn("mongoimport", opts);
-  }  
+    return spawn;
+  }
 
 };
